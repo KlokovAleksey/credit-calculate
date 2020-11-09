56c6b805117099717fb4122497050414ddee58a9
Aleksey Klokov <lex-oz@yandex.ru>
2020-11-09 04:14:11 +0300

    fixed a bug

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public long calculate(long sum, int month) {
-        double monthPercent = 9.99 / 12;
+        double monthPercent = 9.99 / 12 / 100;
         double monthPayment = sum * (monthPercent + monthPercent / (Math.pow((1 + monthPercent), month) - 1));
         return (long) monthPayment;
     }
